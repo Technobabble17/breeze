a8e97d46ac3b2bc0f52ae0e4bb5a4cb4bf60d587
Technobabble17 <58636640+Technobabble17@users.noreply.github.com>
2021-07-08 13:52:27 -0600

    Base line w/Tailwind

--- resources/views/addresses/index.blade.php
+++ resources/views/addresses/index.blade.php
@@ -1,34 +1,31 @@
-<!doctype html>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Address Book') }} {{--i18n https://laravel.com/docs/8.x/localization --}}
+        </h2>
+        <a href="{{ route('addresses.create') }}">New Address</a>
+    </x-slot>
 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8"> {{--Rem is Root not relative (dont use em)--}}
+            <?php foreach ($addresses as $address) : ?> {{--Tailwind docs: padding6, each unit is a quarter rem (6 is 1.5rem/24px~) --}}
+                <div class="mb-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <article class="p-6 bg-white border-b border-gray-200">
 
+                        <h2 class="text-2xl bold">{{ $address->lastname, $address->firstname }}</h2>
+                        <a class="text-blue-600 hover:border-b hover:border-blue-600" href="{{ route('addresses.show',['address'=> $address->id]) }}">View</a> |
 
-<title> Address Book </title>
-<link rel="stylesheet" href="/app.css">
+                        <a class="text-blue-600 hover:border-b hover:border-blue-600" href="{{ route('addresses.edit',['address'=> $address->id]) }}">Edit address</a> |
 
-<body>
-    <nav>
-        <a href="{{ route('posts.index') }}"> Blog Page <br></a>
-        <a href="{{ route('addresses.create') }}"> Add </a>
-    </nav>
+                        <form class=" inline" action="{{ route('addresses.destroy',['address'=> $address->id]) }}" method="POST">
+                            <input class="text-blue-600 bg-transparent cursor-pointer hover:border-b hover:border-blue-600" type="submit" value="Delete address" />
+                            @method('delete')
+                            @csrf
+                        </form>
 
-
-
-    <?php foreach ($addresses as $address) : ?>
-        <article>
-
-            <h2>{{ $address->lastname, $address->firstname }}</h2>
-           <a href="{{ route('addresses.show',['address'=> $address->id]) }}"> View <br></a>
-
-           <a href="{{ route('addresses.edit',['address'=> $address->id]) }}"> Edit address <br></a>
-
-
-           <form action="{{ route('addresses.destroy',['address'=> $address->id]) }}" method="POST">
-            <input class="btn btn-default" type="submit" value="Delete address" />
-            @method('delete')
-            @csrf
-
-
-        </article>
-
-    <?php endforeach; ?>
-</body>
+                    </article>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</x-app-layout>
